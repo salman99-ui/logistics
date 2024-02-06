98f98cfcd4f8c87738a278c8de471e55b40dc7c0
Salman <slmndamanhuri@gmail.com>
2024-02-06 20:01:22 +0700

    add server

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// const express = require('express')
-const mysql   = require('mysql');
-var connection = mysql.createConnection({
-    host     : 'database-2.clw08wwgm5ok.ap-southeast-2.rds.amazonaws.com',
-    user     : 'admin',
-    port : '3306',
-    password : 'coba1234',
-    database : 'TA'
-    });
+const express = require('express')
+// const mysql   = require('mysql');
+// var connection = mysql.createConnection({
+//     host     : 'database-2.clw08wwgm5ok.ap-southeast-2.rds.amazonaws.com',
+//     user     : 'admin',
+//     port : '3306',
+//     password : 'coba1234',
+//     database : 'TA'
+//     });
     
-    connection.connect((err) => {
-        if (err) {
-            console.log('error connecting: ' + err);
-            return;
-          }
+//     connection.connect((err) => {
+//         if (err) {
+//             console.log('error connecting: ' + err);
+//             return;
+//           }
          
-          console.log('connected');
-    });
+//           console.log('connected');
+//     });
     
-// const app = express()
-// app.get('/', (req,res)=> {
+const app = express()
+app.get('/', (req,res)=> {
+    res.json({
+        data : "Hello"
+    })
+})
 
-    
-
-//     res.json({
-//         data : "Hello"
-//     })
-// })
-
-// app.listen(4000,() => {
-//     console.log("server running")
-// })
+app.listen(4000,() => {
+    console.log("server running")
+})

9d8b693f261c93e5b3bc67d2357e5a2f45bf44a8
salman99-ui <56756690+salman99-ui@users.noreply.github.com>
2024-05-31 00:18:51 +0700

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql'
  const connection = mysql.createConnection({
-    host     : 'database-1.clw08wwgm5ok.ap-southeast-2.rds.amazonaws.com',
+    host     : 'database-2.cjwcywoci4t3.ap-southeast-1.rds.amazonaws.com',
     user     : 'admin',
     port : '3306',
-    password : 'coba123456',
+    password : 'coba12345',
     database : 'MyDb'
 });
 
